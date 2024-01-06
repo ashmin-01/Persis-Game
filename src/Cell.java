6ca98cc1d29b817f805549f4b3a1fa283c905245
ashmin-01 <shaymaalmthiab@gmail.com>
2024-01-06 21:05:35 +0300

    path class modified

--- src/Cell.java
+++ src/Cell.java
@@ -1,7 +1,27 @@
 import java.util.ArrayList;
 public class Cell {
     private int label;
-    private boolean is_protected;
+    private boolean isProtected;
     private ArrayList<Pawn> pawns;
+
+    public Cell(int label) {
+        this.label = label;
+    }
+
+    public boolean isProtected() {
+        return isProtected;
+    }
+
+    public void setProtected(boolean isProtected) {
+        this.isProtected = isProtected;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "label=" + label +
+                ", isProtected=" + isProtected +
+                '}';
+    }
 
 }
--- src/SharedPathsExample.java
+++ src/SharedPathsExample.java
@@ -2,35 +2,76 @@ import java.util.ArrayList;
 
 public class SharedPathsExample {
     public static void main(String[] args) {
-        // Create two ArrayLists of ArrayLists
-        ArrayList<ArrayList<Integer>> arrayList1 = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> arrayList2 = new ArrayList<>();
+        // Create humanPath
+        ArrayList<Cell> humanPath = createHumanPath();
 
-        // Initialize each inner ArrayList with numbering
+        // Mark specific cells as protected in humanPath
+        markCellsAsProtected(humanPath, 32, 38, 21, 15, 66, 4, 49, 55);
+        // create computerPath
+        ArrayList<Cell> computerPath = createComputerPath(humanPath);
+
+        // Print the content of humanPath and computerPath
+        printPath(humanPath, "Human Path");
+        printPath(computerPath, "Computer Path");
+    }
+
+    private static ArrayList<Cell> createHumanPath() {
+        ArrayList<Cell> humanPath = new ArrayList<>();
+
+        // Add the first 7 elements labeled as -1
+        for (int i = 0; i < 7; i++) {
+            humanPath.add(new Cell(-1));
+        }
+
+        // Add cells labeled from 1 to 68
         for (int i = 1; i <= 68; i++) {
-            arrayList1.add(createNumberedArrayList(i, 5));
-            arrayList2.add(createNumberedArrayList(i, 5));
+            humanPath.add(new Cell(i));
         }
 
-        // Print the content of the ArrayLists
-        printArrayLists(arrayList1, arrayList2);
-    }
+        // Add the same cell labeled 1
+        humanPath.add(humanPath.get(7));
 
-    // Create an ArrayList with specified numbering and number of elements
-    private static ArrayList<Integer> createNumberedArrayList(int number, int numElements) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(number);
-        for (int i = 1; i < numElements; i++) {
-            arrayList.add(0);
+        // Add another 7 elements labeled as 0
+        for (int i = 0; i < 7; i++) {
+            humanPath.add(new Cell(0));
         }
-        return arrayList;
+
+        return humanPath;
+    }
+    private static ArrayList<Cell> createComputerPath(ArrayList<Cell> humanPath) {
+        ArrayList<Cell> computerPath = new ArrayList<>();
+
+        // Add the first 7 elements labeled as -1
+        for (int i = 0; i < 7; i++) {
+            computerPath.add(new Cell(-1));
+        }
+        // Copy cells to computerPath with different arrangement
+        computerPath.addAll(humanPath.subList(41, 75));
+        computerPath.addAll(humanPath.subList(7, 41));
+
+        // Add the same cell labeled 35
+        computerPath.add(computerPath.get(7));
+
+        // Add another 7 elements labeled as 0
+        for (int i = 0; i < 7; i++) {
+            computerPath.add(new Cell(0));
+        }
+
+        return computerPath;
     }
 
-    // Print the content of the ArrayLists
-    private static void printArrayLists(ArrayList<ArrayList<Integer>> arrayList1, ArrayList<ArrayList<Integer>> arrayList2) {
-        for (int i = 0; i < arrayList1.size(); i++) {
-            System.out.println("ArrayList1[" + i + "]: " + arrayList1.get(i));
-            System.out.println("ArrayList2[" + i + "]: " + arrayList2.get(i));
+    private static void markCellsAsProtected(ArrayList<Cell> path, int... labels) {
+        for (int label : labels) {
+            Cell cell = path.get(label - 1);
+            cell.setProtected(true);
+        }
+    }
+
+    private static void printPath(ArrayList<Cell> path, String pathName) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.println(pathName + "[" + i + "]: " + path.get(i));
         }
     }
 }
+
+
